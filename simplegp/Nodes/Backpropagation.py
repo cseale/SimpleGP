fixed backprop error (- sign before grad), implemented unit test for backprop

simplegp/Nodes/Backpropagation.py
@@ -6,16 +6,16 @@ class Backpropagation:
 	def __init__( self, X_train, y_train ):
 		self.X_train = X_train
 		self.y_train = y_train
-		self.iterations = 5
-		self.learning_rate = 0.5
+		self.iterations = 1000
+		self.learning_rate = 0.01
 
 	def Backprop( self, individual ):
 		# TODO: boolean flag to turn off backprop for comparision runs
 
-		# assume worst fitness possible at start
-		previousFitness = float("inf")
-		previousIndividual = None
-		
+		# Track best individual over all iterations of gradient descent
+		best_indiv = None
+		lowest_mse = float("inf")
+
 		# backwards prop for certain number of iterations
 		# TODO: Check for convergence, and any other early stopping conditions
 		for i in range(self.iterations):
@@ -23,18 +23,13 @@ class Backpropagation:
 			output = individual.GetOutput( self.X_train )
 			mse = np.mean ( np.square( self.y_train - output ) )
 
-			# if there is a degradation, return previou individual
-			if mse > previousFitness:
-				return previousIndividual
-				
-			# mse improved, so keep descending
-			# keep previous value
-			previousFitness = mse
-			previousIndividual = deepcopy(individual)
+			# Track best individual
+			if mse < lowest_mse:
+				best_indiv = deepcopy(individual)
+				lowest_mse = mse
+
 			# hard coded MSE gradient
-			grad_mse = 2 * (self.y_train - output)
+			grad_mse = -2 * (self.y_train - output)
 			# do gradient descent
 			individual.GradientDescent(grad_mse, self.learning_rate)
-
-		return individual
-		
+		return best_indiv